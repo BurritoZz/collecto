d0b905870d656d44cac583032cbbb64f3a9bf4e0
s1726692 <m.a.hendriks@student.utwente.nl>
2020-12-25 21:55:32 +0100

    Wrote findDoubleMoves and did some refactoring

--- app/src/main/java/collecto/controller/Board.java
+++ app/src/main/java/collecto/controller/Board.java
@@ -1,8 +1,12 @@
 package collecto.controller;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 public class Board {
-    private Ball[][] fields; // organised as [row][col]
+    private final Ball[][] fields; // organised as [row][col]
     private final int BOARDSIZE = 7;
 
 	public Board() {
@@ -10,18 +14,18 @@ public class Board {
         generateStart(fields);
 	}
 
-    public Board(Ball[][] fields) {
+    public Board(final Ball[][] fields) {
         this.fields = new Ball[BOARDSIZE][BOARDSIZE];
-        for (int i = 0; i < BOARDSIZE-1; i++) {
-            for (int j = 0; j < BOARDSIZE-1; j++) {
+        for (int i = 0; i < BOARDSIZE; i++) {
+            for (int j = 0; j < BOARDSIZE; j++) {
                 this.fields[i][j] = fields[i][j];
             }
         }
     }
 
-    private void generateStart(Ball[][] fields) {
+    private void generateStart(final Ball[][] fields) {
         // Create list with starting balls
-        ArrayList<Ball> collection = new ArrayList<Ball>();
+        final ArrayList<Ball> collection = new ArrayList<Ball>();
         // Create and add balls to list
         for (int i = 0; i < 8; i++) {
             collection.add(Ball.BLUE);
@@ -35,7 +39,7 @@ public class Board {
         Collections.shuffle(collection);
 
         // Create queue out of list
-        ArrayDeque<Ball> queue = new ArrayDeque<Ball>(collection);
+        final ArrayDeque<Ball> queue = new ArrayDeque<Ball>(collection);
 
         // Assign balls to fields
         for (int i = 0; i < BOARDSIZE; i++) {
@@ -44,14 +48,14 @@ public class Board {
                     fields[i][j] = Ball.NONE;
                     continue;
                 }
-                Ball b = queue.pop();
+                final Ball b = queue.pop();
                 fields[i][j] = b;
             }
         }
 
         ArrayDeque<Integer[]> swaps = findSwaps();
         while (swaps.size() > 0) {
-            for (Integer[] swap : swaps) {
+            for (final Integer[] swap : swaps) {
                 swapWithRandom(swap[0], swap[1]);
             }
             swaps = findSwaps();
@@ -60,17 +64,16 @@ public class Board {
 
     public ArrayDeque<Integer[]> findSwaps() {
          // Create queue containing locations that need to be swapped
-        ArrayDeque<Integer[]> swaps = new ArrayDeque<Integer[]>();
+        final ArrayDeque<Integer[]> swaps = new ArrayDeque<Integer[]>();
 
         // Find and add swap locations
         for (int i = 0; i < BOARDSIZE; i++) {
             for (int j = 0; j < BOARDSIZE; j++) {
-                ArrayList<Integer[]> adjacent = getAdjacent(i, j);
-                for (Integer[] coords : adjacent) {
-                    System.out.println(String.format("%d, %d"));
+                final ArrayList<Integer[]> adjacent = getAdjacent(i, j);
+                for (final Integer[] coords : adjacent) {
                     if (fields[coords[0]][coords[1]] == fields[i][j] && fields[i][j] != Ball.NONE) {
                         boolean toAdd = true;
-                        for (Integer[] swap : swaps) {
+                        for (final Integer[] swap : swaps) {
                             if (swap[0] == i && swap[1] == j) {
                                 toAdd = false;
                             }
@@ -84,13 +87,13 @@ public class Board {
         return swaps;
     }
 
-    private void swapWithRandom(int x, int y) {
-        Random r = new Random();
-        int x2 = r.nextInt(BOARDSIZE);
-        int y2 = r.nextInt(BOARDSIZE);
+    private void swapWithRandom(final int x, final int y) {
+        final Random r = new Random();
+        final int x2 = r.nextInt(BOARDSIZE);
+        final int y2 = r.nextInt(BOARDSIZE);
         if (x2 == 3 && y2 == 3)
             return;
-        Ball t = fields[x][y];
+        final Ball t = fields[x][y];
         fields[x][y] = fields[x2][y2];
         fields[x2][y2] = t;
     }
@@ -108,8 +111,8 @@ public class Board {
         return result;
     }
 
-    private ArrayList<Integer[]> getAdjacent(int i, int j) {
-        ArrayList<Integer[]> result = new ArrayList<Integer[]>();
+    private ArrayList<Integer[]> getAdjacent(final int i, final int j) {
+        final ArrayList<Integer[]> result = new ArrayList<Integer[]>();
         if (i > 0)
             result.add(new Integer[]{i-1, j});
         if (j > 0)
@@ -121,7 +124,7 @@ public class Board {
         return result;
     }
 
-    public void pushUp(int j) {
+    public void pushUp(final int j) {
         for (int i = 1; i < BOARDSIZE; i++) {
             int x = i;
             while (x > 0 && fields[x-1][j] == Ball.NONE) {
@@ -132,7 +135,7 @@ public class Board {
         }
     }
 
-    public void pushDown(int j) {
+    public void pushDown(final int j) {
         for (int i = BOARDSIZE - 2; i >= 0; i--) {
             int x = i;
             while (x < BOARDSIZE - 1 && fields[x+1][j] == Ball.NONE) {
@@ -143,7 +146,7 @@ public class Board {
         }
     }
 
-    public void pushLeft(int i) {
+    public void pushLeft(final int i) {
         for (int j = 1; j < BOARDSIZE; j++) {
             int x = j;
             while (x > 0 && fields[i][j-1] == Ball.NONE) {
@@ -154,7 +157,7 @@ public class Board {
         }
     }
 
-    public void pushRight(int i) {
+    public void pushRight(final int i) {
         for (int j = BOARDSIZE - 1; j >=0; j--) {
             int x = j;
             while (x < BOARDSIZE - 2 && fields[i][x+1] == Ball.NONE) {
@@ -166,10 +169,10 @@ public class Board {
     }
 
     public ArrayList<Ball> retrieveAdjacentBalls() {
-        ArrayList<Ball> result = new ArrayList<Ball>();
-        ArrayDeque<Integer[]> swaps = findSwaps();
+        final ArrayList<Ball> result = new ArrayList<Ball>();
+        final ArrayDeque<Integer[]> swaps = findSwaps();
 
-        for (Integer[] swap : swaps) {
+        for (final Integer[] swap : swaps) {
             if (fields[swap[0]][swap[1]] != Ball.NONE) {
                 result.add(fields[swap[0]][swap[1]]);
                 fields[swap[0]][swap[1]] = Ball.NONE;
@@ -179,70 +182,62 @@ public class Board {
                 fields[swap[2]][swap[3]] = Ball.NONE;
             }
         }
-        printSwaps(swaps);
         return result;
     }
 
+    // TODO:Remove
     public void testBoard() {
         System.out.println(this);
-        System.out.println(findPossibleSingleMoves());
+        System.out.println(findPossibleDoubleMoves());
     }
 
 
-    // TODO: remove
-    private void printSwaps(ArrayDeque<Integer[]> swaps) {
-        for (Integer[] swap : swaps) {
-            System.out.println(String.format("%d, %d - %d, %d", swap[0], swap[1], swap[2], swap[3]));
-        }
-    }
-
-    public String ballListTostring(ArrayList<Ball> ballList) {
+    public String ballListTostring(final ArrayList<Ball> ballList) {
         String res = "";
-        for (Ball ball : ballList) {
+        for (final Ball ball : ballList) {
             res += ball + ", ";
         }
-        return res;
+        return res.substring(0, res.length() - 2);
     }
 
     public String findPossibleSingleMoves() {
         Board copy;
         String res = "";
         ArrayList<Ball> ballList;
-        for (int i = 0; i < BOARDSIZE-1; i++) {
+        for (int i = 0; i < BOARDSIZE; i++) {
             boolean valid = false;
-            for (int j = 0; j < BOARDSIZE-1; j++) {
+            for (int j = 0; j < BOARDSIZE; j++) {
                 if (fields[i][j] == Ball.NONE)
                     valid = true;
             }
             if (!valid)
                 continue;
-            copy = deepcopy();
+            copy = clone();
             copy.pushLeft(i);
             ballList = copy.retrieveAdjacentBalls();
             if  (ballList.size() > 0) {
-                System.out.println(ballList.size());
                 res += "L " + i + " " + ballListTostring(ballList) + "\n";
             }
-            copy = deepcopy();
+            copy = clone();
             copy.pushRight(i);
             ballList = copy.retrieveAdjacentBalls();
             if (ballList.size() > 0)
                 res += "R " + i + " " + ballListTostring(ballList) + "\n";
         }
-        for (int j = 0; j < BOARDSIZE-1; j++) {
+        for (int j = 0; j < BOARDSIZE; j++) {
             boolean valid = false;
-            for (int i = 0; i < BOARDSIZE-1; i++) {
+            for (int i = 0; i < BOARDSIZE; i++) {
                 if (fields[i][j] == Ball.NONE)
                     valid = true;
             }
             if (!valid)
                 continue;
-            copy = deepcopy();
+            copy = clone();
             copy.pushUp(j);
             ballList = copy.retrieveAdjacentBalls();
             if  (ballList.size() > 0)
                 res += "U " + j + " " + ballListTostring(ballList) + "\n";
-            copy = deepcopy();
+            copy = clone();
             copy.pushDown(j);
             ballList = copy.retrieveAdjacentBalls();
             if (ballList.size() > 0)
@@ -251,7 +246,216 @@ public class Board {
         return res;
     }
 
-    public Board deepcopy() {
+    public String findPossibleDoubleMoves() {
+        Board copy1;
+        Board copy2;
+        String res = "";
+        ArrayList<Ball> ballList;
+        for (int i = 0; i < BOARDSIZE; i++) {
+            boolean valid = false;
+            for (int j = 0; j < BOARDSIZE; j++) {
+                if (fields[i][j] == Ball.NONE)
+                    valid = true;
+            }
+            if (!valid)
+                continue;
+            copy1 = clone();
+            copy1.pushLeft(i);
+            for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                boolean valid2 = false;
+                for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushLeft(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("L%d L%d %s\n", i, i2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushRight(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("L%d R%d %s\n", i, i2, ballListTostring(ballList));
+            }
+            for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                boolean valid2 = false;
+                for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushUp(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("L%d U%d %s\n", i, j2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushDown(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("L%d D%d %s\n", i, j2, ballListTostring(ballList));
+            }
+            copy1 = clone();
+            copy1.pushRight(i);
+            for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                boolean valid2 = false;
+                for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushLeft(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("R%d L%d %s\n", i, i2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushRight(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("R%d R%d %s\n", i, i2, ballListTostring(ballList));
+            }
+            for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                boolean valid2 = false;
+                for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushUp(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("R%d U%d %s\n", i, j2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushDown(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("R%d D%d %s\n", i, j2, ballListTostring(ballList));
+            }
+        }
+        for (int j = 0; j < BOARDSIZE; j++) {
+            boolean valid = false;
+            for (int i = 0; i < BOARDSIZE; i++) {
+                if (fields[i][j] == Ball.NONE)
+                    valid = true;
+            }
+            if (!valid)
+                continue;
+            copy1 = clone();
+            copy1.pushUp(j);
+            for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                boolean valid2 = false;
+                for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushLeft(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("U%d L%d %s\n", j, i2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushRight(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("U%d R%d %s\n", j, i2, ballListTostring(ballList));
+            }
+            for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                boolean valid2 = false;
+                for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushUp(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("U%d U%d %s\n", j, j2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushDown(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("U%d D%d %s\n", j, j2, ballListTostring(ballList));
+            }
+            copy1 = clone();
+            copy1.pushDown(j);
+            for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                boolean valid2 = false;
+                for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushLeft(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("D%d L%d %s\n", j, i2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushRight(i2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("D%d R%d %s\n", j, i2, ballListTostring(ballList));
+            }
+            for (int j2 = 0; j2 < BOARDSIZE; j2++) {
+                boolean valid2 = false;
+                for (int i2 = 0; i2 < BOARDSIZE; i2++) {
+                    if (copy1.fields[i2][j2] == Ball.NONE)
+                        valid2 = true;
+                }
+                if (!valid2)
+                    continue;
+                copy2 = copy1.clone();
+                copy2.pushUp(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("D%d U%d %s\n", j, j2, ballListTostring(ballList));
+                copy2 = copy1.clone();
+                copy2.pushDown(j2);
+                ballList = copy2.retrieveAdjacentBalls();
+                if (ballList.size() > 0)
+                    res += String.format("D%d D%d %s\n", j, j2, ballListTostring(ballList));
+            }
+        }
+        return res;
+    }
+
+    public Board clone() {
         return new Board(fields);
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.deepHashCode(fields);
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Board other = (Board) obj;
+		if (!Arrays.deepEquals(fields, other.fields))
+			return false;
+		return true;
+	}
+
 }
